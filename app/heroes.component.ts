8dbf6a0346adf9e0e73c351911fc82373df3fac7
JCGrant <jamescolin.grant@gmail.com>
2016-07-21 17:16:36 +0100

    Can add and delete heroes from heroes component.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,15 +3,19 @@ import { Router } from "@angular/router";
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from "./hero-detail.component";
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls: ['app/heroes.component.css'],
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  addingHero = false;
+  error: any;
 
   constructor(
     private router: Router,
@@ -19,7 +23,31 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService
+      .getHeroes()
+      .then(heroes => this.heroes = heroes)
+      .catch(error => this.error = error);
+  }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+      .delete(hero)
+      .then(res => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+      .catch(error => this.error = error);
   }
 
   ngOnInit() {
