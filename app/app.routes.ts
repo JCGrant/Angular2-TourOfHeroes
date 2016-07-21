3f0222b4b8c4ce8dc39041e2e3301f6435d36554
JCGrant <jamescolin.grant@gmail.com>
2016-07-21 15:25:49 +0100

    Add a simple dashboard view and set it as the default ('/') view.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,21 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
+
 import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from "./dashboard.component";
 
 const routes: RouterConfig = [
   {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
